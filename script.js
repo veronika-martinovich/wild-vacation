073cb486d4a8c3d0ebf9e5fc57d2d4cda2b89d9f
Veronika Martinovich <veranika.mart@gmail.com>
2020-03-09 16:39:26 +0300

    css styles fix

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    let navIcon = document.querySelector('.nav-icon');
-    let navList = document.querySelector('.nav-list');
-    let directionsItems = document.querySelectorAll('.directions__item');
+    let navIcon = document.querySelector('.nav-ico');
+    let navList = document.querySelector('.nav__list');
 
     navIcon.onclick = function () {
-        navList.classList.toggle('nav-list_hidden');
+        navList.classList.toggle('nav__list_hidden');
     }
 })
